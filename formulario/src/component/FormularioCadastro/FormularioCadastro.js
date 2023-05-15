acb54037e214a6c9cfea95751c1b035e0bb031bd
Crisleine Erculano <candidocrislaine576@gmail.com>
2023-05-15 14:49:30 -0300

    Criando Switch Promoções e Novidades.

--- formulario/src/component/FormularioCadastro/FormularioCadastro.js
+++ formulario/src/component/FormularioCadastro/FormularioCadastro.js
@@ -1,23 +1,47 @@
 import React from "react";
-import Button from "@mui/material/Button";
-import TextField from '@mui/material/TextField'
+import { Button, TextField, Switch, FormControlLabel } from "@mui/material";
+
 
 function FormularioCadastro() {
    return (
       <form>
-         <TextField id="nome" label="Nome" margin="normal" fullWidth />
-         
-         <TextField id="sobrenome" label="Sobrenome" margin="normal" fullWidth />
-         
-         <TextField id="cpf" label="CPF" margin="normal" fullWidth />
+         <TextField
+            id="nome"
+            label="Nome"
+            margin="normal"
+            fullWidth
+         />
 
-         {/* <label>Promoções</label>
-         <input type="checkbox" />
-         
-         <label>Novidades</label>
-         <input type="checkbox" /> */}
+         <TextField
+            id="sobrenome"
+            label="Sobrenome"
+            margin="normal"
+            fullWidth
+         />
 
-         <Button type="submit" variant="contained" color="primary">Cadastrar</Button>
+         <TextField
+            id="cpf"
+            label="CPF"
+            margin="normal"
+            fullWidth
+         />
+
+         <FormControlLabel
+            label="Promoções"
+            control={<Switch name="promocao" defaultChecked color="primary" />}
+         />
+
+         <FormControlLabel
+            label="Novidades"
+            control={<Switch name="novidades" color="primary" />}
+         />
+
+         <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+         >Cadastrar
+         </Button>
       </form>
    )
 
