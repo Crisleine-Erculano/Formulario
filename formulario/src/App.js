8bbbbc06dcabdfe184ae924e6f599a9860489570
Crisleine Erculano <candidocrislaine576@gmail.com>
2023-05-15 15:05:10 -0300

    Estilizando Título.

--- formulario/src/App.js
+++ formulario/src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
 import FormularioCadastro from './component/FormularioCadastro/FormularioCadastro';
-import { Container } from '@mui/material';
+import { Container, Typography } from '@mui/material';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 
 class App extends Component {
   render() {
     return (
       <Container>
-        <h1>Formulário de Cadastro</h1>
+        <Typography variant="h3" component="h1" aling="center">Formulário de Cadastro</Typography>
         <FormularioCadastro />
       </Container>
 
