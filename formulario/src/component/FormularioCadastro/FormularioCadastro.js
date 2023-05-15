afaa37f3ec6a564acb30c1615365ec76b5a3b833
Crisleine Erculano <candidocrislaine576@gmail.com>
2023-05-15 14:23:32 -0300

    Estilizando campos do Formulário.

--- formulario/src/component/FormularioCadastro/FormularioCadastro.js
+++ formulario/src/component/FormularioCadastro/FormularioCadastro.js
@@ -5,10 +5,11 @@ import TextField from '@mui/material/TextField'
 function FormularioCadastro() {
    return (
       <form>
-         <TextField id="nome" label="Nome"  />
+         <TextField id="nome" label="Nome" margin="normal" fullWidth />
          
-         <TextField id="sobrenome" label="Sobrenome" />
-         <TextField id="cpf" label="CPF" />
+         <TextField id="sobrenome" label="Sobrenome" margin="normal" fullWidth />
+         
+         <TextField id="cpf" label="CPF" margin="normal" fullWidth />
 
          {/* <label>Promoções</label>
          <input type="checkbox" />
