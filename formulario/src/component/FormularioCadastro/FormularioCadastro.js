9230a53b9796ee1a86cf20633b8af1e19e2ce35c
Crisleine Erculano <candidocrislaine576@gmail.com>
2023-05-18 18:19:10 -0300

    Criando função para renderizar formulário.

--- formulario/src/component/FormularioCadastro/FormularioCadastro.js
+++ formulario/src/component/FormularioCadastro/FormularioCadastro.js
@@ -1,17 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
 import DadosPessoais from "./DadosPessoais";
 import DadosUsuario from "./DadosDoUsuario";
 import DadosEntrega from "./DadosEntrega";
+import { Typography } from '@mui/material';
 
 
 function FormularioCadastro(aoEnviar, validarCpf) {
+   const [etapaAtual, setEtapaAtual] = useState(1)
+
+   function formularioAtual(etapa) {
+   switch (etapa) {
+      case 0:
+         return <DadosUsuario />
+      case 1:
+         return <DadosPessoais aoEnviar={aoEnviar} validarCpf={validarCpf}/>
+      case 2:
+         return <DadosEntrega />
+      default:
+         return <Typography>Erro ao selecionar formulário.</Typography>
+   }
+}
    return (
       <>
-      <DadosPessoais aoEnviar={aoEnviar} validarCpf={validarCpf} />
-      <DadosUsuario />
-      <DadosEntrega />
+         {formularioAtual(etapaAtual)}
       </>
    )
 
 }
+
+//<DadosPessoais  />
+//<DadosUsuario />
 export default FormularioCadastro
