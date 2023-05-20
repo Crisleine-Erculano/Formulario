d62cdbc3299b09e2d2b8e949d616c0656ac76eb4
Crisleine Erculano <candidocrislaine576@gmail.com>
2023-05-20 15:46:47 -0300

    Criando função coletarDados.

--- formulario/src/component/FormularioCadastro/FormularioCadastro.js
+++ formulario/src/component/FormularioCadastro/FormularioCadastro.js
@@ -1,15 +1,22 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import DadosPessoais from "./DadosPessoais";
 import DadosUsuario from "./DadosDoUsuario";
 import DadosEntrega from "./DadosEntrega";
 
 function FormularioCadastro(aoEnviar, validarCPF) {
-   const [etapaAtual, setEtapaAtual] = useState(0)
+   const [etapaAtual, setEtapaAtual] = useState(0);
+   const [dadosColetados, setDados] = useState({});
+
    const formularios = [
-      <DadosUsuario aoEnviar={proximo} />,
-      <DadosPessoais aoEnviar={proximo} validarCPF={validarCPF} />,
-      <DadosEntrega aoEnviar={aoEnviar} />
+      <DadosUsuario aoEnviar={coletarDados} />,
+      <DadosPessoais aoEnviar={coletarDados} validarCPF={validarCPF} />,
+      <DadosEntrega aoEnviar={coletarDados} />
    ]
+   function coletarDados(dados){
+      setDados({...dadosColetados, ...dados})
+      proximo();
+
+   }
 
    function proximo() {
       setEtapaAtual(etapaAtual + 1)
